eee148e15de5f640915d83c8caacb6d24db3230e
Nate Meyer <nmeyer@gmail.com>
2021-12-31 19:40:30 -0800

    check all of the env vars

--- internal/config/main_test.go
+++ internal/config/main_test.go
@@ -1,14 +1,29 @@
 package config
 
 import (
-	"os"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 	cfg := *Config{}.New()
 
-	if cfg["BONUS_ROOM_HUE"] != os.Getenv("BONUS_ROOM_HUE") {
+	if _, ok := cfg["BONUS_ROOM_HUE"]; !ok {
 		t.Errorf("BONUS_ROOM_HUE env var is not set")
+	}
+
+	if _, ok := cfg["BONUS_ROOM_PLEX_CLIENT_NAME"]; !ok {
+		t.Errorf("BONUS_ROOM_PLEX_CLIENT_NAME env var is not set")
+	}
+
+	if _, ok := cfg["HUE_USER"]; !ok {
+		t.Errorf("HUE_USER env var is not set")
+	}
+
+	if _, ok := cfg["PORT"]; !ok {
+		t.Errorf("PORT env var is not set")
+	}
+
+	if _, ok := cfg["REDIS_URL"]; !ok {
+		t.Errorf("REDIS_URL env var is not set")
 	}
 }
